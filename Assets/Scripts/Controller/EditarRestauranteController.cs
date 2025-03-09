1319667d85b3ed1a1fd93b9a91a0205cd8c26dd9
Carlos <carlosferser03@gmail.com>
2025-03-09 22:20:17 +0100

    Canvas para la edición del restaurante casi terminado

--- Assets/Scripts/Controller/EditarRestauranteController.cs
+++ Assets/Scripts/Controller/EditarRestauranteController.cs
@@ -1,10 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class EditarRestauranteController : MonoBehaviour
 {
+    [SerializeField] private TMP_Dropdown horaApertura;
+    [SerializeField] private TMP_Dropdown minutoApertura;
+    [SerializeField] private TMP_Dropdown horaCierre;
+    [SerializeField] private TMP_Dropdown minutoCierre;
+
+
     void Awake()
     {
         SceneManager.LoadSceneAsync("General Controller", LoadSceneMode.Additive);
@@ -13,12 +20,29 @@ public class EditarRestauranteController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+
+        List<string> opcionesHoras = new List<string> { "00", "01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24" };
+        List<string> opcionesMinutos = new List<string> { "00", "01", "02", "03", "04", "05", "06", "07", "08", "09",
+                                                          "10", "11", "12", "13", "14", "15", "16", "17", "18", "19",
+                                                          "20", "21", "22", "23", "24", "25", "26", "27", "28", "29",
+                                                          "30", "31", "32", "33", "34", "35", "36", "37", "38", "39",
+                                                          "40", "41", "42", "43", "44", "45", "46", "47", "48", "49",
+                                                          "50", "51", "52", "53", "54", "55", "56", "57", "58", "59" };
+        AgregarOpcionesADropdown(horaApertura, opcionesHoras);
+        AgregarOpcionesADropdown(minutoApertura, opcionesMinutos);
+        AgregarOpcionesADropdown(horaCierre, opcionesHoras);
+        AgregarOpcionesADropdown(minutoCierre, opcionesMinutos);
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    private void AgregarOpcionesADropdown(TMP_Dropdown dropdown, List<string> opciones)
+    {
+        dropdown.ClearOptions();  // Limpia opciones anteriores
+        dropdown.AddOptions(opciones);
     }
 }
